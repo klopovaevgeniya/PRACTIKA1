2ad183ed0d93a55447bd69a0840d3512935c9165
klopovaevgeniya <isip_e.d.klopova@mpt.ru>
2024-03-31 11:14:48 +0300

    Добавьте файлы проекта.

---
+++ PRACTIKA1/Window1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using PRACTIKA1.TEREMOKDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using PRACTIKA1.TEREMOKDataSetTableAdapters;
+
+namespace PRACTIKA1
+{
+    public partial class Window1 : Window
+    {
+        EMPLOYEETableAdapter employee = new EMPLOYEETableAdapter();
+        BLINITableAdapter blin = new BLINITableAdapter();
+        BUYERSTableAdapter buyer = new BUYERSTableAdapter();
+        public Window1()
+        {
+            InitializeComponent();
+            EmployeeGrid.ItemsSource = employee.GetData();
+            BlinGrid.ItemsSource = blin.GetData();
+            BuyerGrid.ItemsSource = buyer.GetData();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+
+    }
+}
---
+++ PRACTIKA1/Window2.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PRACTIKA1
+{
+    public partial class Window2 : Window
+    {
+        TEREMOKEntities3 context = new TEREMOKEntities3();
+        public Window2()
+        {
+            InitializeComponent();
+            EmployeeGrid.ItemsSource = context.EMPLOYEE.ToList();
+            BlinGrid.ItemsSource = context.BLINI.ToList();
+            BuyerGrid.ItemsSource = context.BUYERS.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+    }
+}
